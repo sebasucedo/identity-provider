0da0956c0cd708e916ec616a05739505d5badd45
sebas <sebasucedo@gmail.com>
2024-11-09 10:28:56 -0300

    Refresh token

--- src/identity-provider.api/Requests.cs
+++ src/identity-provider.api/Requests.cs
@@ -7,4 +7,5 @@ public record NewPasswordRequest(string Username, string NewPassword, string Ses
 public record SignupRequest(string Username, string Email, string Password, string ConfirmationPassword);
 public record ConfirmSignupRequest(string Username, string Code);
 public record TokenRequest(string Username, string Password);
+public record RefreshTokenRequest(string Username, string RefreshToken);
 public record ResendConfirmationRequest(string Username);
